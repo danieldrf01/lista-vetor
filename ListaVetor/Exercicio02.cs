a680fde464a6d456d8a3a7f54039c35b88529020
Alan eduardo alves <alaneduardoalves2018@gmail.com>
2018-04-24 16:15:37 -0300

    Desenvolvimento do Exercício 2 com vetor
    Solicitar 10 nomes e apresentar no final.

--- ListaVetor/Exercicio02.cs
+++ ListaVetor/Exercicio02.cs
@@ -8,5 +8,49 @@ namespace ListaVetor
 {
     class Exercicio02
     {
+        public Exercicio02()
+        {
+            string[] nomes = new string[10];
+
+            Console.Write("\nNome1: ");
+            nomes[0] = Console.ReadLine();
+
+            Console.Write("\nNome2: ");
+            nomes[1] = Console.ReadLine();
+
+            Console.Write("\nNome3: ");
+            nomes[2] = Console.ReadLine();
+
+            Console.Write("\nNome4: ");
+            nomes[3] = Console.ReadLine();
+
+            Console.Write("\nNome5: ");
+            nomes[4] = Console.ReadLine();
+
+            Console.Write("\nNome6: ");
+            nomes[5] = Console.ReadLine();
+
+            Console.Write("\nNome7: ");
+            nomes[6] = Console.ReadLine();
+
+            Console.Write("\nNome8: ");
+            nomes[7] = Console.ReadLine();
+
+            Console.Write("\nNome9: ");
+            nomes[8] = Console.ReadLine();
+            
+            Console.Write("\nNome10: ");
+            nomes[9] = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Nomes: " + nomes[0] + ", " + nomes[1] + ", " + nomes[2] + ", " + nomes[3] + ", " + nomes[4] + ", " + nomes[5] + ", " + nomes[6] + ", " + nomes[7] + ", " + nomes[8] + ", " + nomes[9]);
+
+
+
+
+
+
+        }
     }
 }
--- ListaVetor/Principal.cs
+++ ListaVetor/Principal.cs
@@ -10,7 +10,9 @@ namespace ListaVetor
     {
         static void Main(string[] args)
         {
-            new Exercicio01();
+            //new Exercicio01();
+            new Exercicio02();
+
 
         }
     }
